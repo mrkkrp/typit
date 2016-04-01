9f16445da10e00ee0542c7c13477618f8631841b
mrkkrp <markkarpov@opmbx.org>
2016-04-02 01:36:06 +0600

    Implement basic things

--- Cask
+++ Cask
@@ -2,3 +2,6 @@
 (source melpa)
 
 (package-file "typit.el")
+
+(development
+ (depends-on "f"))
--- typit.el
+++ typit.el
@@ -5,7 +5,7 @@
 ;; Author: Mark Karpov <markkarpov@openmailbox.org>
 ;; URL: https://github.com/typit
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "24.4"))
+;; Package-Requires: ((emacs "24.4") (f "0.18"))
 ;; Keywords: games
 ;;
 ;; This file is not part of GNU Emacs.
@@ -34,6 +34,11 @@
 ;;; Code:
 
 (require 'cl-lib)
+(require 'f)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Settings & variables
 
 (defgroup typit nil
   "Cool typing game similar to tests on 10 fast fingers."
@@ -41,6 +46,146 @@
   :tag    "Typit"
   :prefix "typit-"
   :link   '(url-link :tag "GitHub" "https://github.com/mrkkrp/typit"))
+
+(defface typit-title
+  '((t (:inherit font-lock-contant-face)))
+  "Face used to display Typit buffer title.")
+
+(defface typit-timer
+  '((t (:inherit font-lock-warning-face)))
+  "Face used to display the timer.")
+
+(defface typit-normal-text
+  '((t (:inherit default)))
+  "Face used to display words to type.")
+
+(defface typit-current-word
+  '((t (:inherit highlight)))
+  "Face used to highlight current word.")
+
+(defface typit-correct-char
+  '((t (:foreground "spring green")))
+  "Face used to color correctly typed characters.")
+
+(defface typit-wrong-char
+  '((t (:foreground "firebrick")))
+  "Face used to color incorrectly typed characters.")
+
+(defface typit-statistic
+  '((t (:inherit font-lock-keyword-face)))
+  "Face used to render names of statistical values after typing.")
+
+(defface typit-value
+  '((t (:inherit font-lock-constant-face)))
+  "Face used to render statistical values after typing.")
+
+(defcustom typit-dict "english.txt"
+  "Name of dictionary file to use."
+  :tag  "Dictionary to use"
+  :type '(choice (const :tag "English" "english.txt")))
+
+(defcustom typit-dict-dir
+  (when load-file-name
+    (f-slash (f-join (f-parent load-file-name) "dict")))
+  "Path to directory with collection of dictionaries."
+  :tag  "Directory with dictionary files"
+  :type 'directory)
+
+(defcustom typit-line-length 80
+  "Length of line of words to use."
+  :tag  "Length of line of words"
+  :type 'integer)
+
+(defvar typit--dict nil
+  "Vector of words to use (from most common to least common).
+
+If the value is NIL, it means that no dictionary has been loaded
+yet.")
+
+(defvar typit--dict-file nil
+  "File name of currently loaded dictionary.
+
+If no dictionary is loaded, it's NIL.")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Low-level functions
+
+(defun typit--prepare-dict ()
+  "Make sure that ‘typit--dict’ and ‘typit--dict-file’ are set."
+  (let ((dict-file (f-expand typit-dict typit-dict-dir)))
+    (when (or (not typit--dict-file)
+              (not (f-same? typit--dict-file dict-file)))
+      (setq typit--dict-file dict-file
+            typit--dict
+            (with-temp-buffer
+              (insert-file-contents dict-file)
+              (vconcat
+               (split-string
+                (buffer-substring-no-properties
+                 (point-min)
+                 (point-max))
+                "\n" t "[[:space:]]*")))))))
+
+(defun typit--pick-word (num)
+  "Pick a word from ‘typit--dict’.
+
+Use first NUM words from loaded dictionary (if NUM is bigger than
+length of the dictionary, use all words).  All words in
+‘typit--dict’ have approximately the same probability."
+  (elt typit--dict (random (min num (length typit--dict)))))
+
+(defun typit--generate-line (num)
+  "Generate a line of appropriate length picking random words.
+
+NUM is the number of words to use from loaded dictionary (if NUM
+is bigger than length of the dictionary, use all words).
+
+This uses words from ‘typit--dict’, which should be initialized
+by the time the function is called.  Result is returned as a list
+of strings with assumption that only one space is inserted
+between each word (then total length should be close to
+‘typit-line-length’)."
+  (let ((words nil)
+        (acc   0))
+    (while (< acc typit-line-length)
+      (let ((word (typit--pick-word num)))
+        (setq acc
+              (+ acc
+                 (length word)
+                 (if words 1 0)))
+        (push word words)))
+    (cdr words)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Top-level interface
+
+;;;###autoload
+(defun typit-test (words)
+  "Run typing test with using WORDS most common words from dictionary.
+
+Dictionary is an array of words in ‘typit-dict’.  By default it's
+English words ordered from most common to least common.  You can
+let-bind the variable and change it, it's recommended to use at
+least 1000 words so ‘typit-advanced-test’ could work properly."
+  (interactive "p")
+  ;; TODO write me
+  )
+
+;;;###autoload
+(defun typit-basic-test ()
+  "Basic typing test (top 200 words).
+
+See ‘typit-test’ for more information."
+  (interactive)
+  (typit-test 200))
+
+;;;###autoload
+(defun typit-advanced-test ()
+  "Advanced typing test (top 1000 words)."
+  (interactive)
+  (typit-test 1000))
 
 (provide 'typit)
 
